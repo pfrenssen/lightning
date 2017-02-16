b704d4be6d89a8255aac3f040ddb4c057811e7e4
Adam Hoenich <adam.hoenich@acquia.com>
2017-02-16 15:22:51 -0500

    Renamed StaticImage to ImmutableImage.

--- modules/lightning_features/lightning_media/src/Plugin/Field/FieldWidget/ImmutableImage.php
+++ modules/lightning_features/lightning_media/src/Plugin/Field/FieldWidget/ImmutableImage.php
@@ -6,20 +6,20 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\image\Plugin\Field\FieldWidget\ImageWidget;
 
 /**
- * Plugin implementation of the 'static_image' widget.
+ * Plugin implementation of the 'image_immutable' widget.
  *
  * This widget is identical to the image_image widget it extends, except that it
  * suppresses the Remove button and the link to the uploaded file(s).
  *
  * @FieldWidget(
- *   id = "static_image",
- *   label = @Translation("Static image"),
+ *   id = "image_immutable",
+ *   label = @Translation("Immutable image"),
  *   field_types = {
  *     "image"
  *   }
  * )
  */
-class StaticImage extends ImageWidget {
+class ImmutableImage extends ImageWidget {
 
   /**
    * {@inheritdoc}
@@ -27,13 +27,10 @@ class StaticImage extends ImageWidget {
   public static function process($element, FormStateInterface $form_state, $form) {
     $element = parent::process($element, $form_state, $form);
 
-    // Don't allow the remove button to be displayed.
-    $element['remove_button']['#access'] = FALSE;
-
-    // Suppress the file link.
     foreach ($element['fids']['#value'] as $fid) {
       $element['file_' . $fid]['#access'] = FALSE;
     }
+    $element['remove_button']['#access'] = FALSE;
 
     return $element;
   }
